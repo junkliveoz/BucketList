8b470b9b416159aff49f1d535b3ccf550d3cab93
Adam Sayer <asayer82@icloud.com>
2024-08-14 21:10:15 +1000

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Adam Sayer on 14/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
