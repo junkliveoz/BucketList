0096a4b48a7a5c6a9cbe73d47222dce11a46600b
Adam Sayer <asayer82@icloud.com>
2024-08-15 21:59:05 +1000

    Completed day 70, added user locations to the map, improved map annotations and allwed a user to edit a saved map annotation details

---
+++ BucketList/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by Adam Sayer on 15/8/2024.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
---
+++ BucketList/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  BucketList
+//
+//  Created by Adam Sayer on 15/8/2024.
+//
+
+import Foundation
