8b06a3e237de2cf99812628113146090fd20516f
Adam Sayer <asayer82@icloud.com>
2024-08-15 17:47:34 +1000

    Completed day 69.  Added the touch ID and Face ID excercise

---
+++ BucketList/Excercises/TouchIDFaceID.swift
@@ -0,0 +1,18 @@
+//
+//  TouchIDFaceID.swift
+//  BucketList
+//
+//  Created by Adam Sayer on 15/8/2024.
+//
+
+import SwiftUI
+
+struct TouchIDFaceID: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TouchIDFaceID()
+}
