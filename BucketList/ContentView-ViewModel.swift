ba0925cd58ac55f33024bc5c20417460414e1d4c
Adam Sayer <asayer82@icloud.com>
2024-08-16 16:23:06 +1000

    Completed day 72: Moved the app to an MVVM architecture,  added in Face ID support or the secure data.

---
+++ BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by Adam Sayer on 16/8/2024.
+//
+
+import Foundation
+import MapKit
+
+extension ContentView {
+    @Observable
+    class ViewModel {
+        
+    }
+}
